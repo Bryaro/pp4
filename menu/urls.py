b1043f6bc311c2f9994265b4619b014a5fa19c04
Bryar Osman <bryarosman.bo@gmail.com>
2024-01-31 15:44:23 +0000

    fix bug on header.html to active class on menu button working now

--- menu/urls.py
+++ menu/urls.py
@@ -5,8 +5,6 @@ app_name = 'menu'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
     path('add_menu/', views.add_menu_item, name='add_menu_item'),
     path('menu/', views.menu_list, name='menu_list'),
 ]
