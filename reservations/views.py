aa8b6334287cf68f0fcfb2d39d3bbe7b2624ccd1
Bryar Osman <bryarosman.bo@gmail.com>
2024-02-03 15:47:31 +0000

    create views.py for reservation, add empty reserve_table.html in tempalte inside reservation app

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ReservationForm
+from django.contrib.auth.decorations import login_required
 
-# Create your views here.
+
+@login_required
+def reserve_table(request):
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            reservation = form.save(commit=False)
+            reservation.user = request.user
+            reservation.save()
+            # Redirects to new URL:
+            return redirect('reservations:reservation_success')
+    else:
+        form = ReservationForm()
+    return render(request, 'reservations/reserve_table.html',{'form': form})
