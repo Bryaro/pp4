840003c25dba85d52f52ba8dbaddb17c58d557de
Bryar Osman <bryarosman.bo@gmail.com>
2024-02-05 10:04:23 +0000

    Add reservation date to confirmation page

--- reservations/views.py
+++ reservations/views.py
@@ -11,8 +11,14 @@ def reserve_table(request):
             reservation = form.save(commit=False)
             reservation.user = request.user
             reservation.save()
+            # Display to user their booking info in the confirmation page
+            context = {
+                'number_of_guests': reservation.number_of_guests,
+                'date': reservation.date,
+                'time': reservation.time,
+            }
             # Redirects to new URL:
-            return render(request, 'reservations/reserve_confirmation.html')
+            return render(request, 'reservations/reserve_confirmation.html', context)
     else:
         form = ReservationForm(initial={'name': request.user.username})
     return render(request, 'reservations/reserve_table.html',{'form': form})
